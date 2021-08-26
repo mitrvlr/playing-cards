a14dc47d7f870e1618a06ff232bc43be4353b24d
seongha <mitrvlr@gmail.com>
2021-08-26 23:09:12 +0900

    refactor based components

---
+++ src/components/layouts/Mat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mat = () => {
+  return (
+    <div>
+      <h2>Mat</h2>
+    </div>
+  );
+};
+
+export default Mat;
---
+++ src/components/pages/UiKit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UiKit = () => {
+  return (
+    <div>
+      <h2>ui kit</h2>
+    </div>
+  );
+};
+
+export default UiKit;
