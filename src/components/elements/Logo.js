c544a58b1972918d2b9264abea9d5b80c26f5159
seongha <mitrvlr@gmail.com>
2021-08-28 15:06:03 +0900

    add logo components

---
+++ src/components/elements/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AiTwotoneSound } from 'react-icons/ai';
+
+const Logo = () => {
+  return (
+    <h2 className="logo">
+      Push push
+      <AiTwotoneSound />
+    </h2>
+  );
+};
+
+export default Logo;
