ec89a2add870e589cc6761725b06989737910064
eunji1117 <somsomiii@naver.com>
2021-08-29 11:28:53 +0900

    update sheet, seat

--- src/components/modules/Seat.js
+++ src/components/modules/Seat.js
@@ -1,17 +1,50 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { sheetVersion1, currentSectorInfo } from '../../store/atoms';
 
 
 const ModuleSeat = (props) => {
   const seatRef = useRef(null);
+  const [organizationSheet] = useRecoilState(sheetVersion1);
+  const [currentSector] = useRecoilState(currentSectorInfo);
 
-  const onClick = () => {
+  const deactiveSeatColor = '#f0f0f7';
+  const activeSeatColor = currentSector?.color || '#c9c9d4';
+
+  const onSelectSeat = () => {
     const seatElem = seatRef.current;
-    seatElem.style.backgroundColor = '#C9C9D4';
-    props.selectSeat(props.seatId);
+    props.onSelectSeat(props.seatId);
+
+    if (seatElem.dataset.sectorId) {
+      delete seatElem.dataset.sectorId;
+      seatElem.style.backgroundColor = deactiveSeatColor;
+    } else {
+      seatElem.dataset.sectorId = currentSector.id;
+      seatElem.style.backgroundColor = activeSeatColor;
+    }
   };
 
+  useEffect(() => {
+    const seatElem = seatRef.current;
+    organizationSheet?.map((organization) => {
+      organization.sheet.map((seat) => {
+        if (seat.locate.toString() === seatElem.dataset.seatId) {
+          seatElem.dataset.sectorId = organization.id;
+          seatElem.style.backgroundColor = organization.color;
+        }
+      });
+    });
+  }, [organizationSheet]);
+
   return (
-    <button id={props.seatId} ref={seatRef} className="seat" onClick={onClick}>
+    <button
+      ref={seatRef}
+      className="seat"
+      data-seat-id={props.seatId}
+      style={{ backgroundColor: deactiveSeatColor }}
+      disabled={!currentSector}
+      onClick={onSelectSeat}
+    >
       {/*홍길동*/}
     </button>
   );
