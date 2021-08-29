5c12c96e2ac5e75956643489fb022aa0e4045d41
eunji1117 <somsomiii@naver.com>
2021-08-29 11:25:59 +0900

    update recoil store

--- src/store/atoms.js
+++ src/store/atoms.js
@@ -1,6 +1,28 @@
 import { atom } from 'recoil';
+import { responseOrganizations, responseSheetVersion1 } from '../mock';
 
 export const sheetMap = atom({
   key: 'sheetMap',
-  default: [3, 3],
+  default: [5, 4],
+});
+
+export const currentSectorInfo = atom({
+  key: 'currentSectorInfo',
+  default: null,
+});
+
+// todo: currentSectorInfo 의 sheet 배열이 초기값이 되도록 수정
+export const selectedSeatMap = atom({
+  key: 'selectedSeatMap',
+  default: [],
+});
+
+export const organizationList = atom({
+  key: 'responseOrganizations',
+  default: responseOrganizations,
+});
+
+export const sheetVersion1 = atom({
+  key: 'responseSheetVersion1',
+  default: responseSheetVersion1,
 });
