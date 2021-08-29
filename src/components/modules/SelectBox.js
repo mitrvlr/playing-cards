33bfba38ed741cb4c18415c45e540fe1134ef6ea
eunji1117 <somsomiii@naver.com>
2021-08-29 11:28:18 +0900

    update selectbox

--- src/components/modules/SelectBox.js
+++ src/components/modules/SelectBox.js
@@ -1,46 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ElementButton from '../elements/Button';
+import { BsCheck } from 'react-icons/bs';
 
 const ModuleSelectBox = (props) => {
+  const [selectItem, setSelectItem] = useState(null);
+
+  const onClickItem = (item) => {
+    setSelectItem(item);
+  };
+
+  const onSubmit = () => {
+    props.onSubmit(selectItem);
+  };
 
   return (
     <div className="select-box">
       {props.children}
+
       <ul className="select-box__list">
         {props.list?.map((item) => (
-          <li key={item.id} onClick={() => props.onClickItem(item)}>
+          <li key={item.id} onClick={() => onClickItem(item)}>
             <input
               id={`select-box-${item.id}`}
               className="a11y"
-              type={props.listType || 'checkbox' }
               name={props.listLabel || ''}
+              type={props.listType || 'checkbox' }
             />
-            <label htmlFor={`select-box-${item.id}`}>{item.name}</label>
+            <label htmlFor={`select-box-${item.id}`}>
+              <BsCheck />{item.title}
+            </label>
           </li>
         ))}
       </ul>
 
       {props.notificationLabel && (
-        <p className="select-box__notify" dangerouslySetInnerHTML={ {__html: props.notificationLabel} } />
+        <p
+          className="select-box__notify"
+          dangerouslySetInnerHTML={ {__html: props.notificationLabel} }
+        />
       )}
 
       <div className="btn-set btn-set--content">
-        {props.isCancel && (
-          <ElementButton
-            isCancel
-            className="btn--thin btn--secondary"
-            callback={props.onCancel}
-          />
-        )}
-        {props.isConfirm && (
-          <ElementButton
-            isChecked
-            className="btn--thin btn--secondary"
-            callback={props.onSubmit}
-          >
-            {props.confirmLabel}
-          </ElementButton>
-        )}
+        <ElementButton
+          isChecked
+          disabled={!selectItem}
+          className="btn--thin"
+          callback={onSubmit}
+        >
+          {props.confirmLabel}
+        </ElementButton>
       </div>
     </div>
   );
