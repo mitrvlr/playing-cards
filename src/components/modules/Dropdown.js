4f98a4ffd0a8fb249d8969d4dd5f4d16f9af498a
eunji1117 <somsomiii@naver.com>
2021-08-29 17:13:51 +0900

    remove unused mock data

--- src/components/modules/Dropdown.js
+++ src/components/modules/Dropdown.js
@@ -1,34 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BsChevronDown } from 'react-icons/bs';
 import { AiOutlinePlus } from 'react-icons/ai';
-import ElementSearchbar from '../elements/Searchbar';
-import ModuleSelectBox from './SelectBox';
 
 const ModuleDropdown = (props) => {
-  const [showDropdown, toggleDropdown] = useState(false);
-  const [searchValue, setSearchValue] = useState('');
-
   return (
     <div className="dropdown">
-      <input id={`dropdown__${props.id}`} type="checkbox" className="a11y dropdown__trigger" checked={showDropdown} onChange={(e) => toggleDropdown((state) => !state)} />
+      <input id={`dropdown__${props.id}`} type="checkbox" className="a11y dropdown__trigger" />
       <label htmlFor={`dropdown__${props.id}`} className="dropdown__label btn btn--ghost">
         {props.isAdded && <AiOutlinePlus />}
         {props.label || props.id}
         <BsChevronDown className="icon icon--dropdown" size="1.2rem" />
       </label>
+
       {props.list && (
         <div className="dropdown__content">
-          <ModuleSelectBox list={props.list}>
-            {props.search && (
-              <ElementSearchbar
-                value={searchValue}
-                placeholder="search"
-                onChange={(e) => setSearchValue(e.target.value)}
-                onResetValue={() => setSearchValue('')}
-                onSearchValue={() => {}}
-              />
-            )}
-          </ModuleSelectBox>
+          <ul>
+            {props.list?.length && props.list.map((item) => <li>{item}</li>)}
+          </ul>
         </div>
       )}
     </div>
