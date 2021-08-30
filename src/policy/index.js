1c4d9b2691741388019270623676242e6ec68263
eunji1117 <somsomiii@naver.com>
2021-08-31 08:13:04 +0900

    add policy constant

---
+++ src/policy/index.js
@@ -0,0 +1,38 @@
+export const Organizations = {
+  DEVELOPMENT: 'DEVELOPMENT',
+  COLOSO: 'COLOSO',
+  FASTCAMPUS: 'FASTCAMPUS',
+  SNOWBALL: 'SNOWBALL',
+  LEMONADE: 'LEMONADE',
+};
+
+export const OrganizationIds = {
+  [Organizations.DEVELOPMENT]: 0,
+  [Organizations.COLOSO]: 1,
+  [Organizations.FASTCAMPUS]: 2,
+  [Organizations.SNOWBALL]: 3,
+  [Organizations.LEMONADE]: 4,
+};
+
+export const OrganizationLabels = {
+  [Organizations.DEVELOPMENT]: '개발실',
+  [Organizations.COLOSO]: '콜로소',
+  [Organizations.FASTCAMPUS]: '패스트캠퍼스',
+  [Organizations.SNOWBALL]: '스노우볼',
+  [Organizations.LEMONADE]: '레모네이드',
+};
+
+export const OrganizationColors = {
+  [Organizations.DEVELOPMENT]: 'rgb(230, 206, 255)',
+  [Organizations.COLOSO]: 'rgb(255, 240, 139)',
+  [Organizations.FASTCAMPUS]: 'rgb(255, 201, 202)',
+  [Organizations.SNOWBALL]: 'rgb(200, 219, 255)',
+  [Organizations.LEMONADE]: 'rgb(212, 247, 162)',
+};
+
+export const SheetMode = {
+  SELECT_SECTOR: 'SELECT_SECTOR',
+  ASSIGN_SECTOR: 'ASSIGN_SECTOR',
+  SELECT_MEMBER: 'SELECT_MEMBER',
+
+}
